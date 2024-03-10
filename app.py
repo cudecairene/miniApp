5bd3291a7dbdda78b58f4e0a1228dacde8dde084
cudecairene <162836731+cudecairene@users.noreply.github.com>
2024-03-10 00:06:16 +0000

    Links to spreadsheet

--- app.py
+++ app.py
@@ -1,49 +1,72 @@
 """
 # My first app
-Here's our first attempt at using data to create a table:
 """
-
+# Libraries
 import streamlit as st
 import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
+
+# DATA DE GOOGLE SHEETS
+csv_url = 'https://docs.google.com/spreadsheets/d/{key}/export?format=csv&gid={gid}'
+key = '1YL0nCg1JmwXOC25kymnRPVE7isqOIEI5'
+
+# Arrancando connector
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+# Funciones cacheadas
 @st.cache_data
 def carga_marcas():
-    return pd.read_csv('marcas.csv').set_index('marca')
+    gid = '496417927'
+    marcas_df = pd.read_csv(csv_url.format(key = key, gid = gid))
+    return marcas_df.set_index('Marca')
 
 @st.cache_data
 def carga_preciario():
-    return pd.read_csv('preciario.csv').set_index('Artículo')
+    gid = '1152069563'
+    preciario_df = pd.read_csv(csv_url.format(key = key, gid = gid))
+    return preciario_df.set_index('Artículo')
 
+
+# Cargando la data
+marcas = carga_marcas()
+preciario = carga_preciario()
+
+# Generando listas para dropdown
+lista_marcas = sorted(list(marcas.index))
+lista_articulos = sorted(list(preciario.index))
+
+
+# OTRAS FUNCIONES
 
 # buscador de categoria de marcga
 def buscar_categoria(marca):
-  return  marcas.loc[marca,'categoria']
+  return  marcas.loc[marca,'Categoria']
 
 # buscador de precios
 def buscar_precio(marca, articulo):
   return preciario.loc[articulo,buscar_categoria(marca)]
 
-marcas = carga_marcas()
-preciario = carga_preciario()
 
-lista_marcas = sorted(list(marcas.index))
-lista_articulos = sorted(list(preciario.index))
+# APP DISPLAY
 
-
-st.title('CUDECA - Buscador de precios')
+st.header(':blue[CUDECA] :sunglasses:', divider='rainbow')
+st.title('Buscador de precios')
 
 marca = st.selectbox(label = 'Selecciona Marca', options = lista_marcas, key=None)
 articulo = st.selectbox(label = 'Selecciona el tipo de articulo', options = lista_articulos, key=None)
-st.divider()
+busca = st.button('Busca')
 
-if marca is not None and articulo is not None:
+if busca:
+    st.divider()
     st.write(f'Has seleccionado {marca} - {articulo}')
     col1, col2 = st.columns(2)
     with col1:
-        st.header("Categoria de marca")
-        st.write(f'{buscar_categoria(marca)}')
+        st.write("Categoria de marca")
+        st.header(f'{buscar_categoria(marca)}')
 
     with col2:
-        st.header("Recomendacion")
-        st.write(f'{buscar_precio(marca,articulo)}')
-    
+        st.write("Recomendacion")
+        st.header(f'{buscar_precio(marca,articulo)}')
+  
+
