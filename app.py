c41a1b9bf5befb6057aa4cb5f1e485ad44583173
cudecairene <162836731+cudecairene@users.noreply.github.com>
2024-03-10 11:31:42 +0000

    Screen updates

--- app.py
+++ app.py
@@ -1,29 +1,21 @@
-"""
-# My first app
-"""
-# Libraries
 import streamlit as st
 import pandas as pd
-
 
 # DATA DE GOOGLE SHEETS
 csv_url = 'https://docs.google.com/spreadsheets/d/{key}/export?format=csv&gid={gid}'
 key = '1YL0nCg1JmwXOC25kymnRPVE7isqOIEI5'
 
-
-# Funciones cacheadas
 @st.cache_data
 def carga_marcas():
-    gid = '496417927'
-    marcas_df = pd.read_csv(csv_url.format(key = key, gid = gid))
-    return marcas_df.set_index('Marca')
+   gid = '496417927'
+   marcas_df = pd.read_csv(csv_url.format(key=key, gid=gid))
+   return marcas_df.set_index('Marca')
 
 @st.cache_data
 def carga_preciario():
-    gid = '1152069563'
-    preciario_df = pd.read_csv(csv_url.format(key = key, gid = gid))
-    return preciario_df.set_index('Artículo')
-
+   gid = '1152069563'
+   preciario_df = pd.read_csv(csv_url.format(key=key, gid=gid))
+   return preciario_df.set_index('Artículo')
 
 # Cargando la data
 marcas = carga_marcas()
@@ -33,37 +25,49 @@ preciario = carga_preciario()
 lista_marcas = sorted(list(marcas.index))
 lista_articulos = sorted(list(preciario.index))
 
-
-# OTRAS FUNCIONES
-
-# buscador de categoria de marcga
 def buscar_categoria(marca):
-  return  marcas.loc[marca,'Categoria']
+   return marcas.loc[marca, 'Categoria']
 
-# buscador de precios
 def buscar_precio(marca, articulo):
-  return preciario.loc[articulo,buscar_categoria(marca)]
+   return preciario.loc[articulo, buscar_categoria(marca)]
 
+# Callback functions for buttons
+def on_click_busca():
+   st.session_state['show_results'] = True
+   st.session_state['selected_marca'] = st.session_state['marca']
+   st.session_state['selected_articulo'] = st.session_state['articulo']
+
+def on_click_busca_otro():
+   st.session_state['show_results'] = False
+
+# Initialize session state variables
+if 'show_results' not in st.session_state:
+   st.session_state['show_results'] = False
+if 'marca' not in st.session_state:
+   st.session_state['marca'] = lista_marcas[0]
+if 'articulo' not in st.session_state:
+   st.session_state['articulo'] = lista_articulos[0]
 
 # APP DISPLAY
+st.header('CUDECA', divider='rainbow')
 
-st.header(':blue[CUDECA] :sunglasses:', divider='rainbow')
-st.title('Buscador de precios')
+# Show initial selection interface
+if not st.session_state['show_results']:
+   st.title('Buscador de precios')
+   st.session_state['marca'] = st.selectbox(label='Selecciona Marca', options=lista_marcas, index=lista_marcas.index(st.session_state['marca']))
+   st.session_state['articulo'] = st.selectbox(label='Selecciona el tipo de articulo', options=lista_articulos, index=lista_articulos.index(st.session_state['articulo']))
+   if st.button('Encuéntralo', on_click=on_click_busca):
+       pass
+else:
+   # Show results and the new button
+   st.title('Resultado')
+   st.write(f'Has seleccionado {st.session_state["selected_marca"]} - {st.session_state["selected_articulo"]}')
+   col1, col2 = st.columns(2)
+   with col1:
+       st.header(f':green[{buscar_categoria(st.session_state["selected_marca"])}]')
 
-marca = st.selectbox(label = 'Selecciona Marca', options = lista_marcas, key=None)
-articulo = st.selectbox(label = 'Selecciona el tipo de articulo', options = lista_articulos, key=None)
-busca = st.button('Busca')
+   with col2:
+       st.header(f':green[{buscar_precio(st.session_state["selected_marca"], st.session_state["selected_articulo"])}]')
 
-if busca:
-    st.divider()
-    st.write(f'Has seleccionado {marca} - {articulo}')
-    col1, col2 = st.columns(2)
-    with col1:
-        st.write("Categoria de marca")
-        st.header(f':green[{buscar_categoria(marca)}]')
-
-    with col2:
-        st.write("Recomendacion")
-        st.header(f':green[{buscar_precio(marca,articulo)}]')
-  
-
+   if st.button('Busca otro articulo', on_click=on_click_busca_otro):
+       pass
